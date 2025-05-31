41e1067d53386e536b161337dde760763c7856c9
TamjidBhai <tamtop2000@gmail.com>
2025-05-31 21:17:17 +0600

    changed footer and made mobile responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import FooterToggle from "./components/FooterToggle";
 
 import "./globals.css";
-import "./style.css";
 
 const inter = Inter({
   subsets: ["latin"],
